5c85b4a9c727ff22a0fedc0f779b6fad059fa843
Valronn <48224926+Valronn@users.noreply.github.com>
2019-07-22 23:56:12 +0300

    Update keys.js

--- configurations/keys.js
+++ configurations/keys.js
@@ -1,4 +1,4 @@
 module.exports ={
-    mongoURI: "mongodb+srv://Valronn:cyJZPgwEE4ezZGY@twitterappdb-c91yl.gcp.mongodb.net/test?retryWrites=true&w=majority",
+    mongoURI: "Your key",
     secretOrKey: "One secret to rule them all"
 }
